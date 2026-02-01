e46c9da6d1e79e3c75302f001deb4503c6ce11fb
Kavi-Bharathi-TCS <93303806+Kavi-Bharathi-TCS@users.noreply.github.com>
2026-02-01 20:48:53 +0530

    Multiple route handlers in single middleware (use cases of next() function)

--- src/app.js
+++ src/app.js
@@ -1,17 +1,47 @@
 const express = require("express");
 const app = express();
 
-app.use("/test",(req,res,next) =>{
-    console.log("Middeleware 1");
+app.use("/user",[(req,res,next) =>{
+    //Route Handler 01
+    console.log("Response 01!!");
     next();
+   // res.send("Response 01!!!"); 
+},
+(req,res,next) => {
+    // Route Handler 02
+    console.log("Response 2!!");
+    next();
+   // res.send("Response 2!!")
+},
+(req,res,next) => {
+    // Route Handler 03
+    console.log("Response 3!!");
+    next();
+   // res.send("Response 2!!")
+},
+(req,res) => {
+    // Route Handler 04
+    console.log("Response 4!!");
+    res.send("Response 4!!")
+}]
+)
+
+app.get("/user/:userId/:password",(req,res) =>{
+    res.send(req.params)
 });
 
+app.post("/user",(req,res) =>{
+    res.send("Data saved successfully")
+});
 
-app.use("/hello",(req,res) =>{
-    res.send("req");
-})
+app.patch("/user",(req,res) =>{
+    res.send("Data updated successfully")
+});
 
+app.delete("/user",(req,res) =>{
+    res.send("Data deleted successfully")
+});
 
 app.listen(3000,() => {
-    console.log("We are listening in the port number 3000...., no");
+    console.log("We are listening in the port number 3000....");
 })
